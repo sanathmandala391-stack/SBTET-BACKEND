1167ec33a03449705a1dff9c35eb759612b9c776
sanathmandala391-stack <sanathmandala391@gmail.com>
2026-04-09 14:12:02 +0530

    Update AdminService.java

--- sbtet/src/main/java/com/sbtetAttendance/sbtet/service/AdminService.java
+++ sbtet/src/main/java/com/sbtetAttendance/sbtet/service/AdminService.java
@@ -4,7 +4,7 @@ package com.sbtetAttendance.sbtet.service;
 
 import com.sbtetAttendance.sbtet.model.*;
 import com.sbtetAttendance.sbtet.Repository.*;
-import com.sbtetAttendance.sbtet.Service.*;
+import com.sbtetAttendance.sbtet.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
