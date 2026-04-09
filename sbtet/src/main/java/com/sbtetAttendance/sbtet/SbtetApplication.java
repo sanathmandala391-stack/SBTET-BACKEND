138846cfe981758e3e134bc47b263be36d547446
sanathmandala391-stack <sanathmandala391@gmail.com>
2026-04-09 07:41:04 +0530

    Update SbtetApplication.java2

--- sbtet/src/main/java/com/sbtetAttendance/sbtet/SbtetApplication.java
+++ sbtet/src/main/java/com/sbtetAttendance/sbtet/SbtetApplication.java
@@ -2,7 +2,7 @@ package com.sbtetAttendance.sbtet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class SbtetApplication {
